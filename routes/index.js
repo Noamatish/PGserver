e240a63b91880a5e5d8c242697e5979b195d26fb
noamatish <noamatish@gmail.com>
2022-11-04 19:13:56 +0200

    cors

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,9 @@
 var express = require("express");
-const app = express();
 const WL = require("./address");
 var router = express.Router();
 
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+var app = express();
+app.use(cors());
 
 /* POST home page. */
 router.post("/", function (req, res, next) {
