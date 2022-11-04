824eac30a393d7c836cc01d04638e871a5e79512
noamatish <noamatish@gmail.com>
2022-11-04 19:42:49 +0200

    cors

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const WL = require("./address");
 const router = express.Router();
 const app = express();
 
-const cors = require("cors");
-
-var corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+app.use(cors());
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
+app.use(bodyParser.json({ limit: "100mb" }));
+// var corsOptions = {
+//   origin: "*",
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 // app.use(cors(corsOptions));
 
-app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));
 
 /* POST home page. */
 router.post("/", function (req, res, next) {
