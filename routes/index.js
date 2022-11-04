cfe8926bf9e8f563a83fa97d4617d0d6f967a750
noamatish <noamatish@gmail.com>
2022-11-04 19:04:04 +0200

    n

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  console.log(WL);
-  res.render("index", { title: "Express" });
+  const { address } = res.body;
+  if (!address) res.send("Eroor").status(500);
+  const found = WL.find(address);
+  if (!found) res.send("Eroor").status(500);
+  else res.json(true);
 });
 
 module.exports = router;
