b76c8a385ea2bd7385e9a7083e60a7d65638196a
noamatish <noamatish@gmail.com>
2022-11-04 19:05:06 +0200

    post

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,19 @@ var express = require("express");
 const WL = require("./address");
 var router = express.Router();
 
-/* GET home page. */
+/* POST home page. */
 router.post("/", function (req, res, next) {
   const { address } = res.body;
   if (!address) res.send("Eroor").status(500);
   const found = WL.find(address);
   if (!found) res.send("Eroor").status(500);
   else res.json(true);
+});
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  console.log(WL);
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
