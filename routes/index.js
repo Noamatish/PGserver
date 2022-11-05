b2e8251551a54f8dc0df3a5117a0fa44f4614975
noamatish <noamatish@gmail.com>
2022-11-05 03:01:18 +0200

    c

--- routes/index.js
+++ routes/index.js
@@ -21,26 +21,15 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  console.log("i, here");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader("Access-Control-Allow-Methods", "POST");
-
-  // Request headers you wish to allow
-  // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "X-Requested-With,content-type"
-  // );
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
+app.use((req, res, next) => {
+  console.log("im here");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH");
   next();
 });
 
